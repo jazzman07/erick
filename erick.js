75ebb1a32e63d146d33ead3985b13bb119c4c136
Bernardo Aparicio <bernardo.contato2020@gmail.com>
2023-05-12 19:11:51 -0300

    Update erick.js

--- erick.js
+++ erick.js
@@ -1,8 +1,8 @@
 var b = document.querySelector(".no");
 b.addEventListener("click", change);
 function change(){
-    var i = Math.floor(Math.random()*500)+1;
-    var j = Math.floor(Math.random()*500)+1;
+    var i = Math.floor(Math.random()*400)+1;
+    var j = Math.floor(Math.random()*400)+1;
     b.style.left = i+"px";
     b.style.top = j+"px";
 }
